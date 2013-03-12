4bd98e110f73b9683114e05aafad162ddffaf4de
Skand Hurkat <sh953@cornell.edu>
2013-03-12 19:23:59 -0400

    Additional tests in vector-vs-array
     - Attempt to incorporate Daniel Lemire's "fair" implementation
     - Attempt to use emplace_back instead of push_back
     - (un)fair implementation that uses pointer to underlying array
    
    This version attempts to fix the initial criticisms that I received on my
    first version. Most significant was the critique that I was comparing a
    static data structure with a dynamic data structure (I still maintain that
    my data structure did not grow with added elements, so the comparison was
    "fair". As an attempt to increase speed, I tried emplace_back instead of
    push_back to prevent initialisation+copy. Finally, I pit Daniel's version
    against mine, using vector::resize() to resize the vector, and using
    C++11's for all loop semantics to insert elements.
    
    The last implementation is a cheap hack that attempts to use a pointer to
    set all values directly, using vector::data(), bypassing the STL
    specifications. However, this is not found to introduce any significant
    gains as compared to the for all loop initialisation.

--- vector-vs-array.cpp
+++ vector-vs-array.cpp
@@ -24,32 +24,33 @@
 
 using namespace std;
 
-int main(void)
+int main(void) 
 {
-  // Begin C test. Fill declare array, fill it up, and repeat 10 times to
-  // get average time taken to fill up 1000000 integers
   chrono::steady_clock::time_point start, end;
-  int* array;
   chrono::duration<double> time_span_cumulative = 
     chrono::duration<double>::zero();
+
+  // Begin C test. Declare array, fill it up, and repeat 10 times to get
+  // average time taken to fill up 1000000 integers
+  int* array;
   for(int i = 0; i < 1000; i++)
   {
     start = chrono::steady_clock::now();
-//    array = reinterpret_cast<int*>(malloc(1000000*sizeof(int)));
     array = new int[1000000];
     if(array == nullptr)
     {
       cerr << "Fatal error with memory allocation" << endl;
       return -1;
     }
-    for(int j = 0; j < 1000000; j++)
+    register int* arrayitr = array;
+    register int j = 0;
+    do
     {
-      array[j] = j;
-    }
+      *arrayitr++ = j++;
+    }while(j < 1000000);
     end = chrono::steady_clock::now();
     time_span_cumulative += 
       chrono::duration_cast<chrono::duration<double>>(end-start);
-//    free(array);
     delete[] array;
   }
   cout << "The C like implementation took "
@@ -62,16 +63,74 @@ int main(void)
   {
     start = chrono::steady_clock::now();
     v.reserve(1000000);
-    for(int j = 0; j < 1000000; j++)
+    for(register int j = 0; j < 1000000; j++)
     {
-      v.push_back(j);
+      v.emplace_back(j);
     }
     end = chrono::steady_clock::now();
     time_span_cumulative += 
       chrono::duration_cast<chrono::duration<double>>(end-start);
+    if(v.capacity() != 1000000)
+    {
+      cerr << "Vector capacity changed to " << v.capacity() << endl;
+      return 1;
+    }
     vector<int>().swap(v);
   }
   cout << "The C++ implementation took "
+       << time_span_cumulative.count()/1000 << " seconds" << endl;
+
+  // Now, the so called "fair" C++ implementation using resize instead of
+  // reserve
+  time_span_cumulative = chrono::duration<double>::zero();
+  vector<int>().swap(v);
+  for(int i = 0; i < 1000; i++)
+  {
+    start = chrono::steady_clock::now();
+    v.resize(1000000);
+    register int j = 0;
+    for(auto& e:v)
+    {
+      e = j++;
+    }
+    end = chrono::steady_clock::now();
+    time_span_cumulative += 
+      chrono::duration_cast<chrono::duration<double>>(end-start);
+    if(v.capacity() != 1000000)
+    {
+      cerr << "Vector capacity changed to " << v.capacity() << endl;
+      return 1;
+    }
+    vector<int>().swap(v);
+  }
+  cout << "The \"fair\" C++ implementation took "
+       << time_span_cumulative.count()/1000 << " seconds" << endl;
+
+  // Try to optimise the "fair" C++ implementation, but this is basically a
+  // hack to reduce the data structure back to C like array.
+  time_span_cumulative = chrono::duration<double>::zero();
+  vector<int>().swap(v);
+  for(int i = 0; i < 1000; i++)
+  {
+    start = chrono::steady_clock::now();
+    v.resize(1000000);
+    register int* iter = v.data();
+    register int j = 0;
+    do
+    {
+      *iter++ = j++;
+    }while(j < 1000000);
+    end = chrono::steady_clock::now();
+    time_span_cumulative += 
+      chrono::duration_cast<chrono::duration<double>>(end-start);
+    if(v.capacity() != 1000000)
+    {
+      cerr << "Vector capacity changed to " << v.capacity() << endl;
+      return 1;
+    }
+    vector<int>().swap(v);
+  }
+  cout << "The (un)fair C++ implementation took "
        << time_span_cumulative.count()/1000 << " seconds" << endl;
   return 0;
 }
